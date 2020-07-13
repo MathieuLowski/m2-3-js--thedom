bb5147201572ab92501781754da86ed07a6608f8
Mathieu Szydlowski <mathieu.lowski@gmail.com>
2020-07-13 01:21:30 -0400

    forgot to push that one

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,36 @@
-// Add your code here!
+const main = document.querySelector('main');
+
+const title = document.createElement('h1');
+const smallTitle = document.createElement('h2');
+const intro = document.createElement('p');
+const robin = document.createElement('img');
+const rating = document.createElement('p');
+const partOne = document.createElement('p');
+const parTwo = document.createElement('p');
+const info_ref = document.createElement('a');
+
+title.innerText='The best How I Met Your Mother episode (according to fans)';
+intro.innerText='As impossible as it seems to name just 1 of the 208legendary episodes as the “best”, the fans havespoken and the highest rated episode is… wait for it…';
+smallTitle.innerText='The Slap Bet (Season 2, Episode 9)';
+rating.innerText='IMDB Rating: 9.5';
+robin.src = './images/robin-sparkles.jpg';
+partOne.innerText='In this episode, Ted is learning a few secrets aboutRobin, namely that she has an unexplained aversionto the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.';
+parTwo.innerText='In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- availablefor all of eternity (horrible call Barney).';
+info_ref.href= 'https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/';
+info_ref.innerText='Source';
+
+main.appendChild(title);
+main.appendChild(intro);
+main.appendChild(smallTitle);
+main.appendChild(rating);
+main.appendChild(robin);
+main.appendChild(partOne);
+main.appendChild(parTwo);
+main.appendChild(info_ref);
+
+const styles= document.createElement('link');
+styles.rel ='stylesheet';
+styles.href = 'styles.css';
+document.head.appendChild(styles);
+
+
--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,38 @@
 // Preset values
 const FROGS = 3;
 
+const track = document.getElementById("track");
+
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
+//We create 3 list element (1 for each racer) and appending them fo rendering in server
+for (let laneCount = 1; laneCount <= FROGS; laneCount++) {
+  let lane = document.createElement("li"); //create the lane
+  track.appendChild(lane);
+  let number = document.createElement("span"); //create lane number
+  number.innerText = laneCount; //adds li number at begining. If FROGS++ === li++
+  lane.appendChild(number);
+  lane.id = `lane-${laneCount}`;
+}
 
-// 2. Create li
+let racers = [];
+for (let i = 0; i < FROGS; i++) {
+  const newFrog = frogStable[i]; //accessing frogStable file linked is .html
+  racers.push(newFrog); //adds frog at the end of racers array
+}
+console.log(racers);
 
-// 3. Create span and add it to the li
-
-// 4. Assign an id to each lane
+racers.forEach(function (racer, id) {
+  let newFrog = document.createElement("span");
+  newFrog.innerText = `${racer.number}`;
+  newFrog.classList.add("frog");
+  newFrog.style.background = racer.color;
+  document.getElementById(`frog-${id + 1}`).appendChild(newFrog);
+  recers[id].progress = 0;
+  racers[is].lane = `frog-${id + 1}`;
+  frogProgress.id = racers[id].name;
+  document.getElementById(`frog-${id + 1}`).appendChild(frogProgress);
+});
+racers.forEach(function (racer) {
+  // call this function once for each racer.
+  racingFrog(racer);
+});
